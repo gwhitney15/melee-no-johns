52d1a99e26d8030b179417e31bdc74ed8873f866
gwhitney15 <gwhitney115@gmail.com>
2023-06-03 04:15:00 -0500

    heroku fix

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const discord = require("discord.js");
 require("discord-reply");
 const client = new discord.Client();
-require("dotenv").config("/.env");
+require("dotenv").config();
 const token = process.env.SECRET_TOKEN;
 client.on("ready", () => {
   console.log(client.user.tag);
